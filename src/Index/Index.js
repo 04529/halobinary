6f9ad356919016dbc4fa9fb7905f9c35b5f9a110
04529 <ishrat.lallmamode@gmail.com>
2023-01-02 02:00:27 -0700

    adding code

--- src/Index/Index.js
+++ src/Index/Index.js
@@ -1,16 +1,22 @@
 import React from "react";
 import './index.css'; 
-import Binary from './Binary';
 
 function Index() {
     return(
-        <div>
-            <span class="gameHeader">HALO</span>
-            <Binary />
-            <span class="container">
-                <button class="btn-1"><span><a href="./start.html">START GAME</a></span></button>
-                <button class="btn-1"><span>CREDITS</span></button>
-            </span>
+        <div class="landingPage">
+            <div class="gameHeader">HALO</div>
+            <div class="binary">
+                    <span style={{'--i':1}}>B</span>
+                    <span style={{'--i':2}}>I</span>
+                    <span style={{'--i':3}}>N</span>
+                    <span style={{'--i':4}}>A</span>
+                    <span style={{'--i':5}}>R</span>
+                    <span style={{'--i':6}}>Y</span>
+            </div>
+                <div class="container">
+                    <button class="btn-1"><span><a href="./start.html">START GAME</a></span></button>
+                    <button class="btn-1"><span>CREDITS</span></button>
+                </div>
     </div>
        
     );
